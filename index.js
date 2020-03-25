d83b38a04894286e67f0f72b595ecea572765ac8
proweb4all <proweb4all@narod.ru>
2020-03-25 16:58:46 +0300

    add MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
@@ -23,6 +24,17 @@ app.use('/card', cardRoutes)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}...`)
-})
+async function start() {
+  try {
+    const url = `mongodb+srv://user:User99@cluster0-hwzqn.mongodb.net/test?retryWrites=true&w=majority`
+    await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})  
+    // useNewUrlParser: true
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}...`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
