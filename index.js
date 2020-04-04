d973371b000fe54d68685e243319f167be1ce93b
ProWeb4All <proweb4all@narod.ru>
2020-04-04 22:37:25 +0300

    5.3 - Auth

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const coursesRoutes = require('./routes/courses')
 const addRoutes = require('./routes/add')
 const ordersRoutes = require('./routes/orders')
+const authRoutes = require('./routes/auth')
 const User = require('./models/user')
+const varMiddleware = require('./middleware/variables')
 
 const app = express()
 const hbs = exphbs.create({
@@ -18,22 +21,21 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
-app.use(async (req, res, next) => {
-  try {
-    const user = await User.findById('5e82435ba4e6901eacd1d68d')
-    req.user = user
-    next()
-  } catch (e) {
-    console.log(e)
-  }
-})
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: false}))
+app.use(session({
+  secret: 'some secret value',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(varMiddleware)
 app.use('/', homeRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/add', addRoutes)
 app.use('/card', cardRoutes)
 app.use('/orders', ordersRoutes)
+app.use('/auth', authRoutes)
 
 
 const PORT = process.env.PORT || 3000
@@ -46,15 +48,15 @@ async function start() {
       useUnifiedTopology: true,
       useFindAndModify: false
     })  
-    const candidate = await User.findOne()
-    if (!candidate) {
-      const user = new User({
-        email: 'yurytaratov@narod.ru',
-        name: 'Yury',
-        cart: {items: []}
-      })
-      await user.save()
-    }
+    // const candidate = await User.findOne()
+    // if (!candidate) {
+    //   const user = new User({
+    //     email: 'yurytaratov@narod.ru',
+    //     name: 'Yury',
+    //     cart: {items: []}
+    //   })
+    //   await user.save()
+    // }
     // useNewUrlParser: true
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}...`)
---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+const {Router} = require('express')
+const User = require('../models/user')
+const router = Router()
+
+router.get('/login', async (req, res) => {
+  res.render('auth/login', {
+    title: 'Авторизация',
+    isLogin: true
+  })
+})
+
+router.get('/logout', async (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/auth/login#login')
+  })
+})
+
+router.post('/login', async (req, res) => {
+  const user = await User.findById('5e82435ba4e6901eacd1d68d')
+  req.session.user = user
+  req.session.isAuthenticated = true
+  req.session.save(err => {
+    if (err) {
+      throw err
+    } 
+    res.redirect('/')
+  })
+})
+
+module.exports = router
