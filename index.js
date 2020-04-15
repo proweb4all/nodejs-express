d71104cbcba6ee13f1aede1fff4245199f366f87
ProWeb4All <proweb4all@narod.ru>
2020-04-16 01:14:38 +0300

    6-2

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ const ordersRoutes = require('./routes/orders')
 const authRoutes = require('./routes/auth')
 const varMiddleware = require('./middleware/variables')
 const userMiddleware = require('./middleware/user')
-const MONGODB_URI = `mongodb+srv://user:User99@cluster0-hwzqn.mongodb.net/shop`
-
+const keys = require('./keys')
 const app = express()
 const hbs = exphbs.create({
   defaultLayout: 'main',
@@ -23,7 +22,7 @@ const hbs = exphbs.create({
 })
 const store = new MongoStore({
   collection: 'sessions',
-  uri: MONGODB_URI
+  uri: keys.MONGODB_URI
 })
 
 app.engine('hbs', hbs.engine)
@@ -33,7 +32,7 @@ app.set('views', 'views')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: false}))
 app.use(session({
-  secret: 'some secret value',
+  secret: keys.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   store
@@ -54,7 +53,7 @@ const PORT = process.env.PORT || 3000
 
 async function start() {
   try {
-    await mongoose.connect(MONGODB_URI, {
+    await mongoose.connect(keys.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
---
+++ keys/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB_URI: `mongodb+srv://user:User99@cluster0-hwzqn.mongodb.net/shop`,
+  SESSION_SECRET: 'some secret value',
+  SENDGRID_API_KEY: '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'
+}
