0e3bec511b587d4de7c7b97147e70877ce453e87
ProWeb4All <33453060+proweb4all@users.noreply.github.com>
2020-04-16 22:52:32 +0300

    Add files via upload

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   MONGODB_URI: `mongodb+srv://user:User99@cluster0-hwzqn.mongodb.net/shop`,
   SESSION_SECRET: 'some secret value',
-  SENDGRID_API_KEY: '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',
   EMAIL_FROM: 'proweb4all@yandex.ru',
   BASE_URL: 'http://localhost:3000'
 }
